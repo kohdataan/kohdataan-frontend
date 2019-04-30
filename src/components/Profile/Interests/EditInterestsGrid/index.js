3c0e701ea4f0f80abe90db566a278a5b5b6625ee
pninja <pmkuosmanen@gmail.com>
2019-04-30 12:17:11 +0300

    fixed styles for interest grid

--- src/components/Profile/Interests/EditInterestsGrid/index.js
+++ src/components/Profile/Interests/EditInterestsGrid/index.js
@@ -1,20 +1,25 @@
 import React from 'react'
 import './styles.scss'
 import propTypes from 'prop-types'
+import ButtonContainer from '../../../ButtonContainer'
 
 const EditInterestsGrid = props => {
   const { interestList } = props
   return (
     <div className="interests-grid">
       {interestList.map(interest => (
-        <span key={interest.key} className="interests-grid-edit-item">
+        <ButtonContainer
+          key={interest.key}
+          className="interests-grid-edit-button interests-grid-edit-item"
+          onClick={() => console.log(`clicked ${interest.key}`)}
+        >
           <i
             aria-hidden="true"
             className={interest.icon}
             title={interest.key}
           />
           <span className="interests-grid-label">{interest.key}</span>
-        </span>
+        </ButtonContainer>
       ))}
     </div>
   )
