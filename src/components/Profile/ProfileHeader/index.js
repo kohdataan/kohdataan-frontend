3136da7798c11158bc57efb4b42eb27bcb3e5cc7
Your Name <karoliina.mietola@helsinki.fi>
2019-09-12 22:11:43 +0300

    button and action for direct messaging

--- src/components/Profile/ProfileHeader/index.js
+++ src/components/Profile/ProfileHeader/index.js
@@ -1,12 +1,18 @@
 import React, { memo } from 'react'
 import './styles.scss'
+import {
+  getMe,
+  getProfiles as getProfilesAction,
+  getProfilesInChannel as getProfilesInChannelAction,
+} from 'mattermost-redux/actions/users'
 
 const Header = props => {
-  const { nickname, location } = props || ''
+  const { nickname, location, user, loggedInUser, startDirect } = props || ''
 
   return (
     <span className="profile-header-item">
       <h1>{nickname}</h1>
+      <button onClick={startDirect}>Aloita keskustelu</button>
       <div>{location}</div>
     </span>
   )
