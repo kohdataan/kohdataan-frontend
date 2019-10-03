b123b6c569161c2f4e97c136d20abf008bb5f4f3
Laura <laura.reivinen@gmail.com>
2019-10-03 10:51:21 +0300

    added event handler for handling login

--- src/containers/LogInContainer.js
+++ src/containers/LogInContainer.js
@@ -1,8 +1,33 @@
 import React, { memo } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { login } from 'mattermost-redux/actions/users'
 import LogIn from '../components/LogIn'
+import * as API from '../api/user'
+import { addUserToState } from '../store/user/userAction'
 
-const LogInContainer = () => {
-  return <LogIn />
+const LogInContainer = props => {
+  const { login: matterMostLogin } = props
+  const handleLogin = async (email, password) => {
+    try {
+      const user = { email, password }
+      let loginSuccess = false
+      await API.userLogin(user).then(res => {
+        localStorage.setItem('userId', res.user.id)
+        localStorage.setItem('authToken', res.token)
+        loginSuccess = true
+      })
+      if (loginSuccess) {
+        await matterMostLogin(email, password)
+      }
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.error(e)
+    }
+  }
+
+  return <LogIn handleLogin={handleLogin} />
 }
 
 // TODO: refactor
@@ -15,4 +40,21 @@ const shouldComponentUpdate = (props, prevProps) => {
   return JSON.stringify(rest) === JSON.stringify(prest)
 }
 
-export default memo(LogInContainer, shouldComponentUpdate)
+LogInContainer.propTypes = {
+  login: PropTypes.func.isRequired,
+  addUserToState: PropTypes.func.isRequired,
+}
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      login,
+      addUserToState,
+    },
+    dispatch
+  )
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(memo(LogInContainer, shouldComponentUpdate))
