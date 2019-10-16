fc9dfbc7e26c90f92c57ac9c10e1e9fe8fbcb1a9
Laura <laura.reivinen@gmail.com>
2019-10-16 21:48:13 +0300

    created handler for registration

--- src/containers/CreateAccountContainer.js
+++ src/containers/CreateAccountContainer.js
@@ -1,8 +1,49 @@
 import React, { memo } from 'react'
+import uniqid from 'uniqid'
 import CreateAccount from '../components/CreateAccount'
+import * as API from '../api/user'
+import InfoPage from '../components/RegistrationFlow/InfoPage'
+import RegistrationContainer from './RegistrationContainer'
 
 const CreateAccountContainer = () => {
-  return <CreateAccount />
+  const handleAccountCreation = async (
+    firstname,
+    lastname,
+    birthdate,
+    email,
+    password,
+    rulesAccepted
+  ) => {
+    // create unique username for Mattermost
+    // Mattermot username must begin with a letter and contain between 3 and 22 characters
+    // including numbers, lowercase letters, and the symbols ".", "-", and "_".
+    let username = `${uniqid.process()}`.concat(email.split('@')[0])
+
+    if (username.length > 22) {
+      username = username.slice(0, 22)
+    }
+    try {
+      const user = {
+        firstname,
+        lastname,
+        birthdate,
+        email,
+        username,
+        password,
+        rulesAccepted,
+      }
+      if (user.rulesAccepted) {
+        await API.userSignUp(user)
+      } else {
+        console.log('Sinun on hyväksyttävä palvelun säännöt.')
+      }
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.error(e)
+    }
+  }
+
+  return <CreateAccount handleAccountCreation={handleAccountCreation} />
 }
 
 // TODO: refactor
