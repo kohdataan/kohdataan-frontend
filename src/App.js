9a6e7e84704e5c45bd2bc0913361913f4ccdbf5f
Laura <laura.reivinen@gmail.com>
2019-10-31 09:54:50 +0200

    pulled changes from dev branch and changed h1 fonts to Fjalla One

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import ChatContainer from './containers/ChatContainer'
 import LogInContainer from './containers/LogInContainer'
 import CreateAccountContainer from './containers/CreateAccountContainer'
 import RegistrationContainer from './containers/RegistrationContainer'
+import RegistrationSuccessContainer from './containers/RegistrationSuccessContainer'
 import ProfileContainer from './containers/ProfileContainer'
 import PasswordResetContainer from './containers/PasswordResetContainer'
 import getInterestsAction from './store/interest/interestAction'
@@ -63,6 +64,10 @@ class App extends Component {
       <Container className="main-container">
         <Route path="/login" component={LogInContainer} />
         <Route path="/reset-password" component={PasswordResetContainer} />
+        <Route
+          path="/registration-message"
+          component={RegistrationSuccessContainer}
+        />
         <Route path="/createaccount" component={CreateAccountContainer} />
         <Route path="/registration/:step" component={RegistrationContainer} />
         <PrivateRoute exact path="/" component={GroupsContainer} />
--- src/components/RegistrationSuccessMessage.js/index.js
+++ src/components/RegistrationSuccessMessage.js/index.js
@@ -1,4 +1,4 @@
-import React, { useState, memo } from 'react'
+import React, { memo } from 'react'
 import { Link } from 'react-router-dom'
 import './styles.scss'
 
