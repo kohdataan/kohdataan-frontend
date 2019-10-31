820c31c108fc547409a4f5a221cf7315aa3391c4
Laura <laura.reivinen@gmail.com>
2019-10-31 09:55:20 +0200

    created container for registration success message

---
+++ src/containers/RegistrationSuccessContainer.js
@@ -0,0 +1,15 @@
+import React, { memo } from 'react'
+import RegistrationSuccessMessage from '../components/RegistrationSuccessMessage.js'
+
+const RegistrationSuccessContainer = () => {
+  return <RegistrationSuccessMessage />
+}
+
+// TODO: refactor
+const shouldComponentUpdate = (props, prevProps) => {
+  const { match: pMatch, ...prest } = prevProps
+  const { match, ...rest } = props
+  return JSON.stringify(rest) === JSON.stringify(prest)
+}
+
+export default memo(RegistrationSuccessContainer, shouldComponentUpdate)
