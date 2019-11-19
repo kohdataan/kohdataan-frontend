8da782e05b553a402cb32db40dcaec93abc171c8
Laura <laura.reivinen@gmail.com>
2019-11-19 13:04:40 +0200

    created separate component for input fields that use validation

---
+++ src/components/ValidatedInputField/index.js
@@ -0,0 +1,51 @@
+import React, { memo } from 'react'
+import PropTypes from 'prop-types'
+
+const ValidatedInputField = React.forwardRef((props, ref) => {
+  const {
+    label,
+    showLabel,
+    ariaInvalid,
+    ariaDescribedBy,
+    inputClassName,
+    labelClassName,
+    type,
+  } = props
+  return (
+    <label htmlFor={label} className={inputClassName}>
+      {showLabel && label}
+      <input
+        type={type}
+        name={label}
+        ref={ref}
+        id={label}
+        aria-label={label}
+        aria-invalid={ariaInvalid}
+        aria-describedby={ariaDescribedBy}
+        placeholder={label}
+        className={labelClassName}
+      />
+    </label>
+  )
+})
+
+ValidatedInputField.propTypes = {
+  type: PropTypes.string,
+  label: PropTypes.string.isRequired,
+  showLabel: PropTypes.bool,
+  ariaInvalid: PropTypes.bool,
+  ariaDescribedBy: PropTypes.string,
+  inputClassName: PropTypes.string,
+  labelClassName: PropTypes.string,
+}
+
+ValidatedInputField.defaultProps = {
+  type: 'text',
+  showLabel: true,
+  ariaInvalid: false,
+  ariaDescribedBy: '',
+  inputClassName: '',
+  labelClassName: '',
+}
+
+export default memo(ValidatedInputField)
