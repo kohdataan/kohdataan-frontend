64441f811a732f14adc53b331def44de1e6332b2
TerriFin <saukkonensami@gmail.com>
2019-11-29 13:23:55 +0200

    Added logout button and associated functionality

--- src/api/user.js
+++ src/api/user.js
@@ -16,6 +16,22 @@ const userLogin = async data => {
   }
 }
 
+const userLogout = async token => {
+  const uri = process.env.REACT_APP_NODE_BACKEND_URL
+  try {
+    const resp = await fetch(`${uri}/auth/logout`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    return handleFetchErrors(resp)
+  } catch (e) {
+    throw new Error(e)
+  }
+}
+
 const userSignUp = async data => {
   const uri = process.env.REACT_APP_NODE_BACKEND_URL
   try {
@@ -152,6 +168,7 @@ const getInterestsByUsername = async (token, username) => {
 
 export {
   userLogin,
+  userLogout,
   userSignUp,
   resetPassword,
   getUser,
