fd34779829dda2e97335fd0301e02a9c9da09718
Laura <laura.reivinen@gmail.com>
2019-12-05 12:19:56 +0200

    added error validation to login view

--- src/components/LogIn/index.js
+++ src/components/LogIn/index.js
@@ -1,42 +1,107 @@
-import React, { useState, memo } from 'react'
+import React, { memo } from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
-import InputField from '../InputField'
-import ButtonContainer from '../ButtonContainer'
+import useForm from 'react-hook-form'
+import ValidatedInputField from '../ValidatedInputField'
 import './styles.scss'
 
 const LogIn = props => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
   const { handleLogin } = props
+  const { register, handleSubmit, errors, setError } = useForm()
+
+  const onSubmit = async data => {
+    try {
+      const response = await handleLogin(data.email.trim(), data.password)
+      if (!response) {
+        setError(
+          'email',
+          'pattern',
+          'Tarkista, että kirjoitit käyttäjätunnuksen oikein.'
+        )
+        setError(
+          'password',
+          'pattern',
+          'Tarkista, että kirjoitit salasanan oikein.'
+        )
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <main className="login-container">
       <h1 className="main-title">Kohdataan</h1>
       <div className="login-fields-container">
         <h2 className="login-title">KIRJAUTUMINEN</h2>
-        <div className="login-input-fields-container">
-          <InputField
-            label="Sähköposti"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-            inputClassName="login-input-text"
-            labelClassName="login-input-field"
-          />
-          <InputField
-            label="Salasana"
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-            inputClassName="login-input-text"
-            labelClassName="login-input-field"
-            type="password"
-          />
-          <ButtonContainer
-            className="login-button"
-            onClick={() => handleLogin(email, password)}
+        <form
+          className="login-input-fields-container"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <div className="formfield-container">
+            <ValidatedInputField
+              label="Sähköposti"
+              name="email"
+              ref={register({
+                required: true,
+                pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+              })}
+              ariaInvalid={!!errors.email}
+              inputClassName="create-account-input-text"
+              labelClassName={
+                errors.email
+                  ? 'create-account-errors-field'
+                  : 'create-account-input-field'
+              }
+            />
+            <div className="error-text">
+              {errors.email &&
+                errors.email.type === 'required' &&
+                'Kirjoita sähköpostiosoite.'}
+              {errors.email &&
+                errors.email.type === 'pattern' &&
+                'Tarkista, että kirjoitit käyttäjätunnuksen oikein.'}
+            </div>
+          </div>
+
+          <div className="formfield-container">
+            <ValidatedInputField
+              label="Salasana"
+              name="password"
+              ref={register({
+                required: true,
+                maxLength: 30,
+                // password must contain lower and upper case letters and numbers
+                pattern: /^(?=.*[0-9]+.*)(?=.*[a-zäöüß]+.*)(?=.*[A-ZÄÖÜ]+.*)[\w\W]{10,}$/,
+              })}
+              type="password"
+              ariaInvalid={!!errors.password}
+              inputClassName="create-account-input-text"
+              labelClassName={
+                errors.password
+                  ? 'create-account-errors-field'
+                  : 'create-account-input-field'
+              }
+            />
+            <div className="error-text">
+              {errors.password &&
+                errors.password.type === 'required' &&
+                'Kirjoita salasana'}
+              {errors.password &&
+                (errors.password.type === 'pattern' ||
+                  errors.password.type === 'maxLength') &&
+                'Tarkista, että kirjoitit salasanan oikein.'}
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="create-account-button"
+            onKeyPress={handleSubmit(onSubmit)}
+            tabIndex="0"
           >
-            Kirjaudu
-          </ButtonContainer>
-        </div>
+            {'Kirjaudu'}
+          </button>
+        </form>
         <div className="login-links-container">
           <Link className="login-link" to="/reset-password">
             {'Olen unohtanut salasanani'}
