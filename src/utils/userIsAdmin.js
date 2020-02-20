19bd5f57dbcb47d1b621a5ffdf72bdb47f0c542d
pninja <pmkuosmanen@gmail.com>
2020-02-20 15:19:06 +0200

    Adds init team root action to get team members and roles and select current team

--- src/utils/userIsAdmin.js
+++ src/utils/userIsAdmin.js
@@ -1,6 +1,7 @@
 const isAdmin = (userId, profiles) =>
   profiles &&
   profiles[userId] &&
-  profiles[userId].roles.includes('system_admin')
+  (profiles[userId].roles.includes('system_admin') ||
+    profiles[userId].roles.includes('team_admin'))
 
 export default isAdmin
